db70035b1a91a12810625474848794aa68da65b5
EXPua1 <161503064+EXPua1@users.noreply.github.com>
2024-10-28 22:49:09 +0200

    text

--- src/redux/contactsSlice.js
+++ src/redux/contactsSlice.js
@@ -1,32 +1,32 @@
 import { createSlice, createSelector } from "@reduxjs/toolkit";
 
+// Початковий стан
 const INITIAL_STATE = {
   contacts: [],
 };
 
+// Створення Slice для контактів
 const contactsSlice = createSlice({
   name: "contacts",
   initialState: INITIAL_STATE,
   reducers: {
     addContact: (state, action) => {
       state.contacts.push(action.payload);
-      localStorage.setItem("contacts", JSON.stringify(state.contacts)); // Зберігайте контакти в localStorage
     },
     deleteContact: (state, action) => {
       state.contacts = state.contacts.filter(
         (contact) => contact.id !== action.payload
       );
-      localStorage.setItem("contacts", JSON.stringify(state.contacts)); // Оновіть localStorage після видалення
     },
   },
 });
 
-// Экспорт редюсера, экшенов и селектора
+// Експорт редюсера та екшенів
 export const contactsReducer = contactsSlice.reducer;
 export const { addContact, deleteContact } = contactsSlice.actions;
 
-// Мемоізований селектор
+// Мемоізований селектор для отримання контактів
 export const selectContacts = createSelector(
-  (state) => state.contacts.contacts, // Селектор для отримання контактів
-  (contacts) => contacts || [] // Повертає масив контактів або порожній масив
+  (state) => state.contacts.contacts,
+  (contacts) => contacts || []
 );
